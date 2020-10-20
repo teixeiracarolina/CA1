a7e6059e57254de382ace6b32bbbcc7f300975fe
teixeiracarolina <goncalves.carolina@outlook.com>
2020-10-20 09:25:14 +0100

    First changes.

---
+++ include/table.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>C A R O L I N A T E I X E I R A</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<?php include('header.php');?>
+
+<div class="container">
+  <h2>COUNTRIES AND CAPITALS</h2>
+  <p>Data to be used on the form.</p>            
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>COUNTRY</th>
+        <th>CAPITAL</th>
+        </tr>
+    </thead>
+    <tbody>
+    
+      <tr>
+        <td>Austria</td>
+        <td>Vienna</td>
+       
+      </tr>
+      
+        <tr>
+        <td>Belgim</td>
+        <td>Brussels</td>
+       
+      </tr>
+      
+        <tr>
+        <td>Cyprus</td>
+        <td>Nicosia</td>
+       
+      </tr>
+      
+      <tr>
+        <td>Czech Republic</td>
+        <td>Prague</td>
+       
+      </tr>
+      <tr>
+        <td>Denmark</td>
+        <td>Copenhagem</td>
+        
+      </tr>
+      <tr>
+        <td>Estonia</td>
+        <td>Tallin</td>
+        
+      </tr>
+      
+            <tr>
+        <td>Finland</td>
+        <td>Helsinki</td>
+        
+      </tr>
+      
+        <tr>
+        <td>France</td>
+        <td>Paris</td>
+        
+      </tr>
+      
+            <tr>
+        <td>Germany</td>
+        <td>Berlin</td>
+        
+      </tr>
+      
+            <tr>
+        <td>Greece</td>
+        <td>Athens</td>
+        
+      </tr>    
+      
+      
+            <tr>
+        <td>Hungary</td>
+        <td>Budapest</td>
+        
+      </tr>
+      
+            <tr>
+        <td>Ireland</td>
+        <td>Dublin</td>
+        
+      </tr>
+      
+      
+            <tr>
+        <td>Italy</td>
+        <td>Rome</td>
+        
+      </tr>      
+           <tr>
+        <td>Latvia</td>
+        <td>Riga</td>
+        
+      </tr>
+      
+           <tr>
+        <td>Lithuania</td>
+        <td>Vilnius</td>
+        
+      </tr>
+      
+           <tr>
+        <td>Luxembourg</td>
+        <td>Luxembourg</td>
+        
+      </tr>      
+           <tr>
+        <td>Malta</td>
+        <td>Valetta</td>
+        
+      </tr>
+      
+           <tr>
+        <td>Netherlands</td>
+        <td>Amsterdam</td>
+        
+      </tr>
+      
+           <tr>
+        <td>Poland</td>
+        <td>Warsaw</td>
+        
+      </tr>
+           <tr>
+        <td>Portugal</td>
+        <td>Lisbon</td>
+        
+      </tr>
+      
+           <tr>
+        <td>Slovakia</td>
+        <td>Bratislava</td>
+        
+      </tr>      
+      
+           <tr>
+        <td>Slovenia</td>
+        <td>Ljubljana</td>
+        
+      </tr>
+      
+         <tr>
+        <td>Spain</td>
+        <td>Madrid</td>
+        
+      </tr>
+            
+         <tr>
+        <td>Sweden</td>
+        <td>Stockholm</td>
+        
+      </tr>
+      
+         <tr>
+        <td>United Kingdom</td>
+        <td>London</td>
+        
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
+
+<?php include('footer.php');?>
